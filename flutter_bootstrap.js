31e827b0514fdc3c2345ca17574abd00983c3d11
Marcos Lameira <mlameiranet@gmail.com>
2026-09-08 00:45:48 -0300

    V1.6: Cabeçalho/Rodapé ajustados, animações restauradas e créditos premium

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2834608291" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3065272845" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
