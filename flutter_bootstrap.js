ebee42bc326879b1deff26ddd0b4a8def19f0c57
Marcos Lameira <mlameiranet@gmail.com>
2026-09-08 00:18:19 -0300

    V1.5: Cabeçalho, Rodapé institucional, Texto justificado e fix créditos

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4138506634" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2834608291" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
